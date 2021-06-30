b732b5a7321d956ff8dcb11f72d9cf537de93999
mhmadwrekat <>
2021-06-30 15:28:45 +0000

    update

--- app.js
+++ app.js
@@ -2,15 +2,15 @@
 var name = prompt("whats your name ?")
 alert("Welcome to my page " + name)
 
-var favSport = prompt("whats Your favorite sport ? foot ball ?basketball ? body building ?")
+var favSport = prompt("please Type Number Of Your Favorite sport ? 1. football ? 2. basketball ? 3. body building ?")
 
-if (favSport == "football")
+if (favSport == "1")
 {
   alert("You Can Find About Football In First Paragraph :)")
-}else if (favSport == "body building")
+}else if (favSport == "2")
 {
   alert("You Can Find About body building In Second Paragraph :)")
-}else if (favSport == "basketball")
+}else if (favSport == "3")
 {
   alert("   You Can Find About basketball In Third Paragraph :)")
 }else {
@@ -18,15 +18,15 @@ if (favSport == "football")
 }
 
 
-if (favSport == "football")
+if (favSport == "1")
 {
-  document.write("<p>"+"   It is A Family Of Team Sports And It fun"+"</p>")
-}else if (favSport == "body building")
+  document.write("<p>"+"  ( The Football ) It is A Family Of Team Sports And It fun"+"</p>")
+}else if (favSport == "2")
 {
-  document.write("<p>"+"   It is A Helpful Sports And For Strong People"+"</p>")
-}else if (favSport == "basketball")
+  document.write("<p>"+"  ( The Body Builders ) It is A Helpful Sports And For Strong People"+"</p>")
+}else if (favSport == "3")
 {
-  document.write("<p>"+"   It is A fun Sportes And For Talled People"+"</p>")
+  document.write("<p>"+"  ( The Basketball ) It is A fun Sportes And For Talled People"+"</p>")
 }else {
-  document.write("<p>"+"  Choose A Favorite Sport you Lovved :) "+"</p>")
+  document.write("<p>"+"  I Think The Football Is A Very Peautiful Sports TRY IT :) "+"</p>")
 }
