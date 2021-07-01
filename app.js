573d194891d0db2b2c6c3ed67b7c344b7985b41c
mhmadwrekat <>
2021-07-01 13:15:44 +0000

    creating css file

--- app.js
+++ app.js
@@ -1,32 +1,70 @@
 
-var name = prompt("whats your name ?")
+/*var name = prompt("whats your name ?")
 alert("Welcome to my page " + name)
+*/
 
-var favSport = prompt("whats Your favorite sport ? foot ball ?basketball ? body building ?")
+var favSport = prompt("Choose Number => 1. football ? 2. body building ? 3. basketball ? 4. Walking ?")
 
-if (favSport == "football")
+if (favSport == "1")
 {
-  alert("You Can Find About Football In First Paragraph :)")
-}else if (favSport == "body building")
+  alert("Read paragraph in [ RED COLOR ] :)")
+}
+else if (favSport == "2")
 {
-  alert("You Can Find About body building In Second Paragraph :)")
-}else if (favSport == "basketball")
+  alert("Read paragraph in [ DARKEN COLOR ] :)")
+}
+else if (favSport == "3")
 {
-  alert("   You Can Find About basketball In Third Paragraph :)")
-}else {
+  alert("Read paragraph in [ ORANGE COLOR ] :)")
+}
+else if (favSport == "4")
+{
+  alert("Read paragraph in [ Green COLOR ] :)")
+}
+else {
   alert("  Sorry , You Can Read About Other Sports Here :) ")
 }
 
 
-if (favSport == "football")
+if (favSport == "1")
 {
-  document.write("<p>"+"   It is A Family Of Team Sports And It fun"+"</p>")
-}else if (favSport == "body building")
+  document.write("<p>"+"  ( The Football ) It is A Family Of Team Sports And It fun"+"</p>")
+}else if (favSport == "2")
 {
-  document.write("<p>"+"   It is A Helpful Sports And For Strong People"+"</p>")
-}else if (favSport == "basketball")
+  document.write("<p>"+"  ( The Body Builders ) It is A Helpful Sports And For Strong People"+"</p>")
+}else if (favSport == "3")
 {
-  document.write("<p>"+"   It is A fun Sportes And For Talled People"+"</p>")
+  document.write("<p>"+"  ( The Basketball ) It is A fun Sportes And For Talled People"+"</p>")
 }else {
-  document.write("<p>"+"  Choose A Favorite Sport you Lovved :) "+"</p>")
+  document.write("<p>"+"  I Think The Football Is A Very Peautiful Sports TRY IT :) "+"</p>")
 }
+
+
+var random = Math.floor(Math.random() * 10 ) + 1 ;
+console.log(random)
+var input = prompt ("Train Your Mind => Guess Number In My mind Between 0 and 10 ?")
+var counter = 1
+
+ while (input !== random)
+ {
+    if (input < random)
+   {alert("Bigger")
+       input = prompt (" ): try another number")}
+
+   else if (input > random)
+    {alert("smaller")
+        input = prompt ("): Try another number")}
+        else if (input == random){
+          alert("thats Right " + random + " | Number Of Your Trying : " + counter + " Times :)")
+          break ;
+        }
+        else {
+          break
+        }
+
+    counter += 1
+ }
+
+
+
+
