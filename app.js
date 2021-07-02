a534d4ba8b4af2206efc5894c2f578343af471e6
mhmadwrekat <>
2021-07-02 14:35:32 +0000

    fix

--- app.js
+++ app.js
@@ -1,45 +1,15 @@
-/*var name = prompt("whats your name ?")
-alert("Welcome to my page " + name)
-*/
+
+var stars = prompt("How many Stars you will Rate Us ? ")
 
 
-var favSport = prompt("Choose Number => 1. football ? 2. body building ? 3. basketball ? 4. Walking ?")
+while (stars < 0 || stars > 5 || stars == null || stars == "") { stars = prompt("Must Between 0 And 5 ") }
 
-if (favSport == "1")
-{
-  alert("Read paragraph in [ RED COLOR ] :)")
+
+for (i = 0; i < stars; i++) {
+  if (stars <= 5 && stars >= 0) { document.write('<img src="https://www.pngkit.com/png/full/201-2013815_estrella-amarilla-rate-star-icon.png" class="center" alt="Stars" width="75" height="75">') }
+
 }
-else if (favSport == "2")
-{
-  alert("Read paragraph in [ DARKEN COLOR ] :)")
-}
-else if (favSport == "3")
-{
-  alert("Read paragraph in [ ORANGE COLOR ] :)")
-}
-else if (favSport == "4")
-{
-  alert("Read paragraph in [ Green COLOR ] :)")
-}
-else {
-  alert("  Sorry , But You Can Read About Other Sports Here :) ")
-}
-
-
-if (favSport == "1")
-{
-  document.write("<p>"+"  ( The Football ) It is A Family Of Team Sports And It fun"+"</p>")
-}else if (favSport == "2")
-{
-  document.write("<p>"+"  ( The Body Builders ) It is A Helpful Sports And For Strong People"+"</p>")
-}else if (favSport == "3")
-{
-  document.write("<p>"+"  ( The Basketball ) It is A fun Sportes And For Talled People"+"</p>")
-}else {
-  document.write("<p>"+"  I Think The Football Is A Very Peautiful Sports TRY IT :) "+"</p>")
-}
-
-
+//////// Small Game :)
 var random = Math.floor(Math.random() * 10) + 1;
 console.log(random)
 var input = prompt("Train Your Mind => Guess Number In My mind Between 0 and 10 ?")
@@ -62,7 +32,7 @@ if (input != null) {
       }
     }
     else if (input == random) {
-      alert("thats Right " + random + " | Number Of Your Trying : " + counter + " Times :)")
+      alert("thats Right " + random + " => Number Of Your Trying : " + counter + " Times :)")
       break;
     }
     else {
